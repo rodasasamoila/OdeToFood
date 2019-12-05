a59ba74e60c42baaebcd989820271bf600c75829
Robert Samoila <rsamoila@gtsro.gabriels.net>
2019-12-05 12:44:14 +0200

    Create the git

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,17 @@
+﻿using OdeToFood.Core;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetRestaurantByName(string name);
+        Restaurant GetById(int id);
+        Restaurant Update(Restaurant updatedRestaurant);
+        Restaurant Add(Restaurant newRestaurant);
+        Restaurant Delete(int id);
+        int Commit();
+        int GetCountOfRestaurant();
+    }
+}
---
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using OdeToFood.Data;
+using OdeToFood.Core;
+using Microsoft.Extensions.Logging;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        public string Message { get; set; }
+        private readonly IRestaurantData restaurantData;
+        private readonly ILogger<ListModel> logger;
+        private readonly IConfiguration config;
+        [BindProperty(SupportsGet =true)]
+        public string SearchTerm { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+        public ListModel(IConfiguration config,IRestaurantData restaurantData,ILogger<ListModel> logger)
+        {
+            this.config = config;
+            this.restaurantData = restaurantData;
+            this.logger = logger;
+        }
+        public void OnGet()
+        {
+            logger.LogError("Executing ListModel"); 
+            Message = config["Message"];
+            Restaurants = restaurantData.GetRestaurantByName(SearchTerm);
+        }
+    }
+}
